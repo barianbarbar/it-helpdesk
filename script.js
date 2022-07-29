40a31da5e09f5437236f7b3e97d67091df4ff0c8
barianbarbar <barianbarbar99@gmail.com>
2022-07-29 21:23:09 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbxLzlhDF2BkNOOAFfIaf3BRoLcA7eJdzxXDcE58HnNQPqeyPKaJ-OOoN6v7Hbm1b1Lebw/exec";
+
+const form = document.forms["submit-to-spreadsheet"];
+const timeFrom = document.querySelector("#from");
+const timeTo = document.querySelector("#to");
+const resultTimeEl = document.querySelector("#waktu");
+const alertSuccess = document.querySelector(".alert.success");
+const alertFailed = document.querySelector(".alert.failed");
+
+form.addEventListener("submit", (evt) => {
+    resultTimeEl.value = `${timeFrom.value}-${timeTo.value}`;
+    console.log(resultTimeEl.value)
+    console.log(new FormData(form));
+    evt.preventDefault();
+    fetch(scriptURL, {
+        method: "POST",
+        body: new FormData(form),
+    })
+    .then(() => {
+        window.scrollTo(0, 0);
+        alertSuccess.classList.add("active");
+        setTimeout(() => {
+            alertSuccess.classList.remove("active");
+        }, 4000);
+        form.reset();
+    })
+    .catch(() => {
+        window.scrollTo(0, 0);
+        alertFailed.classList.add("active");
+        setTimeout(() => {
+            alertFailed.classList.remove("active");
+        }, 4000);
+    });
+});
