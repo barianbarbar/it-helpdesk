48bfc6f40e5ca4c5090b701779b0b45b4215c5c5
barianbarbar <barianbarbar99@gmail.com>
2022-07-30 08:17:32 +0700

    update ui ux

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const form = document.forms["submit-to-spreadsheet"];
 const timeFrom = document.querySelector("#from");
 const timeTo = document.querySelector("#to");
 const resultTimeEl = document.querySelector("#waktu");
+
 const alertSuccess = document.querySelector(".alert.success");
 const alertFailed = document.querySelector(".alert.failed");
 
+const buttonSend = document.querySelector(".btn-send");
+const buttonWait = document.querySelector(".btn-wait");
+
 form.addEventListener("submit", (evt) => {
+    buttonSend.classList.add("d-none");
+    buttonWait.classList.remove("d-none");
+
     resultTimeEl.value = `${timeFrom.value}-${timeTo.value}`;
     console.log(resultTimeEl.value)
     console.log(new FormData(form));
@@ -30,5 +37,9 @@ form.addEventListener("submit", (evt) => {
         setTimeout(() => {
             alertFailed.classList.remove("active");
         }, 4000);
+    })
+    .finally(() => {
+        buttonSend.classList.remove("d-none");
+        buttonWait.classList.add("d-none");        
     });
 });
